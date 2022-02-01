a654be7b1f035938d99f0f77cf5a1e603a9cefcf
jay <jay.mm.message@gmail.com>
2022-02-01 22:00:41 +0800

    Add function of runTimer.

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -5,11 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     final String TAG="MainActivity";
     private int seconds = 0;
     private boolean running = false;
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+
+        //..
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+        String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+
+        timeView.setText(time);
+        if (running) {
+            ++seconds;
+        }
+        //..
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
