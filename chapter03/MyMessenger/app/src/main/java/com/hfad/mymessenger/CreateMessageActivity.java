c070b32b90d7ac91c918319ee317a21d32e9df06
jay <jay.mm.message@gmail.com>
2022-05-08 13:10:01 +0800

    Add intent used by Intent.ACTION_WEB_SEARCH.

--- chapter03/MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ chapter03/MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -1,6 +1,7 @@
 package com.hfad.mymessenger;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,16 +17,23 @@ public class CreateMessageActivity extends Activity {
 
     //Call onSendMessage() when the button is clicked
     public void onSendMessage(View view) {
-        EditText messageView = (EditText)findViewById(R.id.message);
-        String messageText = messageView.getText().toString();
-//        Intent intent = new Intent(Intent.ACTION_SEND); intent.setType("text/plain");
+//        EditText messageView = (EditText)findViewById(R.id.message);
+//        String messageText = messageView.getText().toString();
+
+        //Intent intent = new Intent(Intent.ACTION_SEND); intent.setType("text/plain");
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        EditText input = (EditText) findViewById(R.id.message);
+        String query = input.getText().toString();
+        intent.putExtra(SearchManager.QUERY, query);
+
+        //intent.putExtra(Intent.EXTRA_TEXT, messageText);
 //        intent.putExtra(Intent.EXTRA_TEXT, messageText);
 //        String chooserTitle = getString(R.string.chooser);
 //        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
-//        startActivity(chosenIntent);
 
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        intent.putExtra("message", messageText);
+//
+//        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+//        intent.putExtra("message", messageText);
         
         startActivity(intent);
     }
