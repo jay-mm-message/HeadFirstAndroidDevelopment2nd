62b02ba3aec730be5c49efc841cf9b1f1832eb33
jcwang <jcwang@sonicwall.com>
2022-05-10 22:12:54 +0800

    Add check version for notification on the chapter18.

--- chapter18/Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ chapter18/Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -1,9 +1,11 @@
 package com.hfad.joke;
 
+import android.annotation.SuppressLint;
 import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -31,7 +33,7 @@ public class DelayedMessageService extends IntentService {
         }
         String text = intent.getStringExtra(EXTRA_MESSAGE);
         Log.d(TAG, "The message: " + text);
-        //showText(text);
+        showText(text);
     }
 
     @Override
@@ -46,26 +48,33 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        //Create a notification builder
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(android.R.drawable.sym_def_app_icon)
-                .setContentTitle(getString(R.string.question))
-                .setContentText(text)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setVibrate(new long[] {0, 1000})
-                .setAutoCancel(true);
 
-        //Create an action
-        Intent actionIntent = new Intent(this, MainActivity.class);
-        PendingIntent actionPendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(actionPendingIntent);
+        if (Build.VERSION.SDK_INT >= 32) {
+            //TODO
+            Log.d(TAG, "Build.VERSION.SDK_INT " + Build.VERSION.SDK_INT);
+        } else {
+            Log.d(TAG, "Build.VERSION.SDK_INT " + Build.VERSION.SDK_INT);
+            //Create a notification builder
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                    .setContentTitle(getString(R.string.question))
+                    .setContentText(text)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setVibrate(new long[] {0, 1000})
+                    .setAutoCancel(true);
 
-        //Issue the notification
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(NOTIFICATION_ID, builder.build());
+            //Create an action
+            Intent actionIntent = new Intent(this, MainActivity.class);
+            @SuppressLint("UnspecifiedImmutableFlag") PendingIntent actionPendingIntent = PendingIntent.getActivity(
+                    this,
+                    0,
+                    actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(actionPendingIntent);
+
+            //Issue the notification
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(NOTIFICATION_ID, builder.build());
+        }
     }
 }
